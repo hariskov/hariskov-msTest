6cd7d9755ff989abb222e7803504abb7075ce3fb
hariskov <hariskov@proton.me>
2025-11-23 20:29:09 +0200

    fixes

--- UserManagement/src/main/java/bg/petarh/microservices/management/App.java
+++ UserManagement/src/main/java/bg/petarh/microservices/management/App.java
@@ -1,4 +1,4 @@
-package bg.petarh.microservices;
+package bg.petarh.microservices.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
